29583c0f9e3cc81ec5dec48ba6e22fc8cfaf4fc8
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-16 16:47:27 +0300

    remov sass

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,10 @@
+import './Intro.css'
+
 export default function Intro() {
   return (
     <div className="intro container">
       <h1 className="title">Quizzical</h1>
-      <p>Free online trivia game</p>
+      <p className='description'>Free online trivia game</p>
       <button>Start quiz</button>
     </div>
   )
