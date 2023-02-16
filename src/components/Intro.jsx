95e83f6c2e96efa184ae5905f4b2c958a6f23b67
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-16 14:16:27 +0300

    css resets and basics

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
     <div className="intro container">
-      <h1>Quizzical</h1>
+      <h1 className="title">Quizzical</h1>
       <p>Free online trivia game</p>
       <button>Start quiz</button>
     </div>
