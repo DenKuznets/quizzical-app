42a030a44d36293063e425b2ec7330f81de25ca2
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-16 18:29:02 +0300

    intro css&html done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,10 +2,10 @@ import './Intro.css'
 
 export default function Intro() {
   return (
-    <div className="intro container">
-      <h1 className="title">Quizzical</h1>
-      <p className='description'>Free online trivia game</p>
-      <button>Start quiz</button>
+    <div className="Intro">
+      <h1 className="Intro__title">Quizzical</h1>
+      <p className='Intro__description'>Free online trivia game</p>
+      <button className='Intro__btn'>Start quiz</button>
     </div>
   )
 }
