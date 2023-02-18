c23c0d84e00101e076780cdf958a6f954de59740
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-18 13:32:07 +0300

    main css done

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,13 +1,31 @@
+import "./Questions.css";
+
 export default function Questions() {
   return (
-    <div className="Questions">
-      <h1 className="Questions__question">Question</h1>
-      <ul className="Questions__answers">
-        <li className="Questions__answer">answer 1</li>
-        <li className="Questions__answer">answer 2</li>
-        <li className="Questions__answer">answer 3</li>
-        <li className="Questions__answer">answer 4</li>
-      </ul>
+    <div className="questions">
+      <div className="question-card">
+        <h1 className="question-card__question">
+          How would one say goodbye in Spanish?
+        </h1>
+        <ul className="question-card__answers">
+          <li className="question-card__answer">answer 1</li>
+          <li className="question-card__answer"> 2</li>
+          <li className="question-card__answer">answerdadasdas 3</li>
+          <li className="question-card__answer">answer 4</li>
+        </ul>
+      </div>
+      <div className="question-card">
+        <h1 className="question-card__question">
+          How would one say goodbye in Spanish? How would one say goodbye in
+          Spanish?
+        </h1>
+        <ul className="question-card__answers">
+          <li className="question-card__answer">answer 1</li>
+          <li className="question-card__answer"> 2</li>
+          <li className="question-card__answer">answerdadasdas 3</li>
+          <li className="question-card__answer">answer 4</li>
+        </ul>
+      </div>
     </div>
   );
 }
