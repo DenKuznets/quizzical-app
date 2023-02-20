7d64c7a580002ecb2a7a1a7b5a91bf03c308ac07
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-20 13:38:10 +0300

    correct answers placement refactored

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,34 +5,43 @@ import Result from "./Result";
 import { nanoid } from "nanoid";
 
 export default function Questions(props) {
-  const [gameOver, setGameOver] = useState(false);
+  const [gameOver, setGameOver] = useState(() => false);
+  // запоминаем расположение правильных ответов
+  const [correctAnswersPlacement] = useState(() => {
+    const arr = [];
+    for (let i = 0; i < 5; i++) {
+      arr.push(Math.floor(Math.random() * 4));
+    }
+    console.log(arr);
+    return arr;
+  });
 
-  function selectAnswer(e) { 
+  function selectAnswer(e) {
+    const sel = "selected-answer";
     for (let li of e.target.closest("ul").children) {
-      if (e.target === li) e.target.classList.add("selected-answer");
-      else li.classList.remove("selected-answer");
+      if (e.target === li) {
+        e.target.classList.add(sel);
+      }
+      if (li !== e.target && li.classList.contains(sel)) {
+        li.classList.remove(sel);
+      }
     }
   }
 
   function checkAnswers() {
-    console.log('check answers');
+    console.log("check answers");
+    let answers = document.querySelectorAll(".selected-answer");
+    console.log(answers);
+    // сравнить содержимое выбранного ответа
   }
 
-  // функция перемешивающая массив ответов, что бы правильный ответ всегда был на случайной позиции
-  function shuffle(array) {
-    let newArr = [...array];
-    for (let i = newArr.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
-    }
-    return newArr;
-  }
+  const questionCards = props.questions.map((qObj, index) => {
+    const answers = [...qObj.incorrect_answers];
+    // добавляем правильный ответ в запомненное место
+    answers.splice(correctAnswersPlacement[index], 0, qObj.correct_answer);
 
-  const questionCards = props.questions.map((qObj) => {
-    // перемешиваем ответы
-    const answersArray = shuffle([...qObj.incorrect_answers, qObj.correct_answer]);
     // создаем массив элементов <li>ответ</li>
-    const answersListItems = answersArray.map((answer) => {
+    const answersListItems = answers.map((answer) => {
       return (
         <li
           key={nanoid()}
@@ -61,7 +70,11 @@ export default function Questions(props) {
         {gameOver ? (
           <Result correct="3" />
         ) : (
-          <Button onClick={checkAnswers} className="check-answers-btn" text="Check answers" />
+          <Button
+            onClick={checkAnswers}
+            className="check-answers-btn"
+            text="Check answers"
+          />
         )}
       </div>
     </div>
